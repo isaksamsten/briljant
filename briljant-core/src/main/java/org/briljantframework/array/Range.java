91e1c87a86fe38002287dccd070f3236da72229e
Isak Karlsson <isak-kar@dsv.su.se>
2015-07-28 16:12:07 +0200

    Add JavaDoc to Range

--- briljant-core/src/main/java/org/briljantframework/array/Range.java
+++ briljant-core/src/main/java/org/briljantframework/array/Range.java
@@ -1,17 +1,36 @@
 package org.briljantframework.array;
 
 /**
- * <p> {@linkplain #copy()} returns a mutable {@code IntMatrix}
- *
  * @author Isak Karlsson
  */
 public interface Range extends IntArray {
 
+  /**
+   * The start value of this range
+   *
+   * @return the start value
+   */
   int start();
 
+  /**
+   * The end value of this range
+   *
+   * @return the end value
+   */
   int end();
 
+  /**
+   * The step size of this range
+   *
+   * @return the step size
+   */
   int step();
 
+  /**
+   * Determines if the given value is included in the range
+   *
+   * @param value the value
+   * @return true if the given value is included
+   */
   boolean contains(int value);
 }
